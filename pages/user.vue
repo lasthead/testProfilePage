07cfd051155aa8cb8152db1e0d017b574c7bbd58
Denis <last.head@yandex.ru>
2020-01-23 11:33:21 +0300

    Bind user inputs and store

--- pages/user.vue
+++ pages/user.vue
@@ -2,29 +2,46 @@
   <div class="app">
     <b-form-group  label="Your Name:" >
       <b-form-input
-        v-model="name"
+        v-model="userData.name"
         required
         placeholder="Enter name"
       ></b-form-input>
     </b-form-group>
     <b-form-group label="Your Familia:">
       <b-form-input
-        v-model="surname"
+        v-model="userData.surname"
         required
         placeholder="Enter Familia"
       ></b-form-input>
     </b-form-group>
-    <b-btn variant="primary">Save</b-btn>
+    <b-btn @click="onSave" variant="primary">Save</b-btn>
   </div>
 </template>
 
 <script>
+  import { mapActions, mapGetters, mapMutations } from 'vuex'
+
   export default {
-    name: 'Page user',
+    name: 'PageUser',
     data(){
       return{
-        name: '',
-        surname:''
+        userData: {
+          name: '',
+          surname:''
+        }
+      }
+    },
+    computed: {
+      ...mapGetters({ defaultData: 'user/GET_DATA' })
+    },
+    async asyncData({ store }) {
+      return { userData: { ...await store.dispatch('user/LOAD_DATA') }}
+    },
+    methods: {
+      ...mapActions({ saveUserData: 'user/SAVE_DATA' }),
+
+      async onSave() {
+        await this.saveUserData(this.userData)
       }
     }
   }
